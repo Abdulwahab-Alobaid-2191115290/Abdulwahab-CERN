1b3b8307491e39750666e16f4cf15d7145b90f66
Abdulwahab Alobaid <81912560+Abdulwahab-Alobaid-2191115290@users.noreply.github.com>
2023-09-04 14:37:51 +0300

    // Abdulwahab adding description

--- pixeltrack/exparse.py
+++ pixeltrack/exparse.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+# executes parse.py for the source files of alpaka located in src/alpaka/
+
 import subprocess
 import os
 import sys
@@ -8,10 +10,13 @@ import re
 
 # main
 
+# all source files
 fs = [val for sublist in [[os.path.join(i[0], j) for j in i[2]] for i in os.walk('./src/alpaka/')] for val in sublist]
 
+# execluding files in the test directory
 files = [ f for f in fs if "test" not in f]
 
+# calling parse.py for each file
 for f in files:
     p = subprocess.Popen(['./parse.py', f])
     p.wait()
